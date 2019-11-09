bf83ec8637528cf3bff69b9ec4bf14103b75e79e
JonathanEmanuel12 <jonathaanemanuel21@gmail.com>
2019-11-08 22:25:10 -0300

    correcao models

--- models/models.js
+++ models/models.js
@@ -4,6 +4,8 @@ const PacienteMedicacao = require('./PacienteMedicacao');
 const Medico = require('./Medico');
 const Consulta = require('./Consulta');
 
+//para criar as tabelas descomentar os códigos comentados
+
 //criação das tabelas pacientes, medicacoes e paciente-medicacoes
 Paciente.belongsToMany(Medicacao, {through: PacienteMedicacao});
 Medicacao.belongsToMany(Paciente, {through: PacienteMedicacao});
@@ -11,17 +13,18 @@ Medicacao.belongsToMany(Paciente, {through: PacienteMedicacao});
 //Medicacao.sync({force:true});
 //PacienteMedicacao.sync({force:true});
 
-//criação da tabela medicosdo   
+//criação da tabela medicos e consultas 
 Paciente.belongsToMany(Medico, {through: Consulta});
 Medico.belongsToMany(Paciente, {through: Consulta});
-Consulta.sync({force:true});
+//Consulta.sync({force:true});
 //Medico.sync(({force:true}));
 
 const models = {
     Medico: Medico,
     Paciente: Paciente,
     Medicacao: Medicacao,
-    PacienteMedicacao: PacienteMedicacao
+    PacienteMedicacao: PacienteMedicacao,
+    Consulta: Consulta
 };
 
 module.exports = models;
