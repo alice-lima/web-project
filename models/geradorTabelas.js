51360ba478aa2b2b0b6f09a4d62391b04cd479dc
JonathanEmanuel12 <jonathaanemanuel21@gmail.com>
2019-12-10 16:32:58 -0300

    adicao de script sql e script js gerador de tabelas

--- models/geradorTabelas.js
+++ models/geradorTabelas.js
@@ -5,16 +5,26 @@ const Medico = require('./Medico');
 const Consulta = require('./Consulta');
 const Retorno = require('./Retorno');
 
-function criarTabelas() {
+async function criarTabelas() {
     
     Paciente.belongsToMany(Medicacao, {through: Receita});
     Medicacao.belongsToMany(Paciente, {through: Receita});
+    await Paciente.sync({force:true});
+    await Medicacao.sync({force:true});
+
     Paciente.belongsToMany(Medico, {through: Consulta});
     Medico.belongsToMany(Paciente, {through: Consulta});
+
+    await Medico.sync(({force:true}));
+    Receita.sync({force:true});
+    Consulta.sync({force:true});
+
     Paciente.hasMany(Retorno);
     Medico.hasMany(Retorno);
     Retorno.belongsTo(Paciente);
     Retorno.belongsTo(Medico);
+
+    Retorno.sync({force:true});
 
     return {
             Medico: Medico,
---
+++ scriptBanco/contruirBancoAddMedicacoes.sql
@@ -0,0 +1,19 @@
+create database testeweb;
+
+SELECT * FROM testeweb.medicacoes;
+
+insert into medicacoes (classeTerapeutica, viaAdministracao, unidade, createdAt, updatedAt) values("Anti-hipertensivos", "Oral", "Comprimido", curdate(), curdate());
+insert into medicacoes (classeTerapeutica, viaAdministracao, unidade, createdAt, updatedAt) values("Hipolipemiantes", "Oral", "Comprimido", curdate(), curdate());
+insert into medicacoes (classeTerapeutica, viaAdministracao, unidade, createdAt, updatedAt) values("Antidepressivos", "Oral", "Comprimido", curdate(), curdate());
+insert into medicacoes (classeTerapeutica, viaAdministracao, unidade, createdAt, updatedAt) values("Benzodiazepínicos", "Oral", "Comprimido", curdate(), curdate());
+insert into medicacoes (classeTerapeutica, viaAdministracao, unidade, createdAt, updatedAt) values("Anti-convulsivantes", "Oral", "Comprimido", curdate(), curdate());
+insert into medicacoes (classeTerapeutica, viaAdministracao, unidade, createdAt, updatedAt) values("Aspirina", "Oral", "Comprimido", curdate(), curdate());
+insert into medicacoes (classeTerapeutica, viaAdministracao, unidade, createdAt, updatedAt) values("Anti-coagulantes", "Oral", "Comprimido", curdate(), curdate());
+
+insert into medicacoes (classeTerapeutica, viaAdministracao, unidade, createdAt, updatedAt) values("Anti-hipertensivos", "Parenteral", "ml", curdate(), curdate());
+insert into medicacoes (classeTerapeutica, viaAdministracao, unidade, createdAt, updatedAt) values("Hipolipemiantes", "Parenteral", "ml", curdate(), curdate());
+insert into medicacoes (classeTerapeutica, viaAdministracao, unidade, createdAt, updatedAt) values("Antidepressivos", "Parenteral", "ml", curdate(), curdate());
+insert into medicacoes (classeTerapeutica, viaAdministracao, unidade, createdAt, updatedAt) values("Benzodiazepínicos", "Parenteral", "ml", curdate(), curdate());
+insert into medicacoes (classeTerapeutica, viaAdministracao, unidade, createdAt, updatedAt) values("Anti-convulsivantes", "Parenteral", "ml", curdate(), curdate());
+insert into medicacoes (classeTerapeutica, viaAdministracao, unidade, createdAt, updatedAt) values("Aspirina", "Parenteral", "ml", curdate(), curdate());
+insert into medicacoes (classeTerapeutica, viaAdministracao, unidade, createdAt, updatedAt) values("Anti-coagulantes", "Parenteral", "ml", curdate(), curdate());
